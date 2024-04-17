edf56631d08ae41e4d722890dfccd86bc3017fc9
tanvirtbf <tanver018765@gmail.com>
2024-04-17 16:00:08 +0600

    create right section

--- CWP landing page/src/components/Right.jsx
+++ CWP landing page/src/components/Right.jsx
@@ -1,28 +1,30 @@
 const Right = () => {
   return (
     <div className="right-main">
-      <div className="right-main-div">
-        <h4 className="right-left">item</h4>
-        <h4 className="right-right">3</h4>
-      </div>
-      <div className="right-main-div">
-        <h4 className="right-left">qty</h4>
-        <h4 className="right-right">5</h4>
-      </div>
-      <div className="right-main-div">
-        <h4 className="right-left">delivery</h4>
-        <h4 className="right-right">Free</h4>
-      </div>
-      <div className="right-main-div">
-        <h4 className="right-left">item</h4>
-        <h4 className="right-right">3</h4>
-      </div>
-      <div className="right-main-div">
-        <h4 className="right-left">item</h4>
-        <h4 className="right-right">3</h4>
+      <div className="right-row">
+        <div className="right-main-div">
+          <h4 className="right-left">item</h4>
+          <h4 className="right-right">3</h4>
+        </div>
+        <div className="right-main-div">
+          <h4 className="right-left">qty</h4>
+          <h4 className="right-right">5</h4>
+        </div>
+        <div className="right-main-div">
+          <h4 className="right-left">delivery</h4>
+          <h4 className="right-right">Free</h4>
+        </div>
+        <div className="right-main-div">
+          <h4 className="right-left">item</h4>
+          <h4 className="right-right">3</h4>
+        </div>
+        <div className="right-main-div">
+          <h4 className="right-left">item</h4>
+          <h4 className="right-right">3</h4>
+        </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Right
+export default Right;

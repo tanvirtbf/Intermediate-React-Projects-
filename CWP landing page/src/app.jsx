37f0763fbf6fe77741d2accb82d6e5911fe3f0bd
tanvirtbf <tanver018765@gmail.com>
2024-04-17 15:12:23 +0600

    bug fixing on

--- CWP landing page/src/app.jsx
+++ CWP landing page/src/app.jsx
@@ -5,7 +5,7 @@ import Right from "./components/Right";
 const App = () => {
   return (
     <>
-      <div className="image"></div>
+      {/* <div className="image"></div> */}
       <div className="main w-full h-screen flex flex-wrap justify-center items-center bg-cover bg-no-repeat">
         <Form />
         <Right />
--- CWP landing page/src/components/Right.jsx
+++ CWP landing page/src/components/Right.jsx
@@ -1,7 +1,26 @@
 const Right = () => {
   return (
     <div className="right-main">
-      Right Side
+      <div className="right-main-div">
+        <h4 className="right-left">item</h4>
+        <h4 className="right-right">3</h4>
+      </div>
+      <div className="right-main-div">
+        <h4 className="right-left">qty</h4>
+        <h4 className="right-right">5</h4>
+      </div>
+      <div className="right-main-div">
+        <h4 className="right-left">delivery</h4>
+        <h4 className="right-right">Free</h4>
+      </div>
+      <div className="right-main-div">
+        <h4 className="right-left">item</h4>
+        <h4 className="right-right">3</h4>
+      </div>
+      <div className="right-main-div">
+        <h4 className="right-left">item</h4>
+        <h4 className="right-right">3</h4>
+      </div>
     </div>
   )
 }

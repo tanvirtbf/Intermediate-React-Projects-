1e1b18b579ca7bb37674ae2d9c380f7e0f7ca2d4
tanvirtbf <tanver018765@gmail.com>
2024-04-17 21:34:12 +0600

    implement useRef hook

--- Social Media App/social-media-app/src/components/home/Home.jsx
+++ Social Media App/social-media-app/src/components/home/Home.jsx
@@ -1,27 +1,41 @@
+import { useRef } from 'react';
 import styles from './Home.module.css'
 
 const Home = () => {
+  const nameRef = useRef("")
+  const schoolnameRef = useRef("")
+  const goalRef = useRef("")
+  const jobRef = useRef("")
+
   const handleSubmit = (e) => {
     e.preventDefault();
+        const name = nameRef.current.value
+        const schoolname = schoolnameRef.current.value
+        const goal = goalRef.current.value
+        const job = jobRef.current.value
+    addPost({name,schoolname,goal,job})
   };
   return (
     <div className={styles.main}>
-      <form onSubmit={handleSubmit}>
+      <form>
         <div>
           <label htmlFor="name">Name : </label>
-          <input type="text" id="name" placeholder="Enter Your Name : " />
+          <input type="text" id="name" placeholder="Enter Your Name : " ref={nameRef}/>
         </div>
         <div>
           <label htmlFor="schoolname">School : </label>
-          <input type="text" id="schoolname" placeholder="Enter Your School Name : " />
+          <input type="text" id="schoolname" placeholder="Enter Your School Name : " ref={schoolnameRef}/>
         </div>
         <div>
           <label htmlFor="goal">Goal : </label>
-          <input type="text" id="goal" placeholder="Describe Your Goal : " />
+          <input type="text" id="goal" placeholder="Describe Your Goal : " ref={goalRef}/>
         </div>
         <div>
           <label htmlFor="job">Job : </label>
-          <input type="text" id="job" placeholder="Enter Your Job : " />
+          <input type="text" id="job" placeholder="Enter Your Job : " ref={jobRef}/>
+        </div>
+        <div>
+          <button onClick={handleSubmit}>Submit</button>
         </div>
       </form>
     </div>

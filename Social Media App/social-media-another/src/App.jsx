b47bfdfb02cba90c5c331b8bcae3d93bebe11c98
tanvirtbf <tanver018765@gmail.com>
2024-04-18 15:44:19 +0600

    created header section by bootstrap

--- Social Media App/social-media-another/src/App.jsx
+++ Social Media App/social-media-another/src/App.jsx
@@ -1,11 +1,12 @@
 import './App.css'
 import 'bootstrap/dist/css/bootstrap.min.css'
+import Header from './components/Header'
 
 function App() {
 
   return (
     <>
-
+      <Header />
     </>
   )
 }

cae0bb03aeb61b700018d4b9274eb5f73e0ab8e7
tanvirtbf <tanver018765@gmail.com>
2024-04-18 09:22:33 +0600

    start new day

--- Social Media App/social-media-app/src/components/home/Home.jsx
+++ Social Media App/social-media-app/src/components/home/Home.jsx
@@ -1,38 +1,58 @@
-import { useRef } from 'react';
-import styles from './Home.module.css'
+import { useRef } from "react";
+import styles from "./Home.module.css";
 
 const Home = () => {
-  const nameRef = useRef("")
-  const schoolnameRef = useRef("")
-  const goalRef = useRef("")
-  const jobRef = useRef("")
+  const nameRef = useRef("");
+  const schoolnameRef = useRef("");
+  const goalRef = useRef("");
+  const jobRef = useRef("");
 
   const handleSubmit = (e) => {
     e.preventDefault();
-        const name = nameRef.current.value
-        const schoolname = schoolnameRef.current.value
-        const goal = goalRef.current.value
-        const job = jobRef.current.value
-    addPost({name,schoolname,goal,job})
+    const name = nameRef.current.value;
+    const schoolname = schoolnameRef.current.value;
+    const goal = goalRef.current.value;
+    const job = jobRef.current.value;
+    addPost({ name, schoolname, goal, job });
   };
   return (
     <div className={styles.main}>
       <form>
         <div>
           <label htmlFor="name">Name : </label>
-          <input type="text" id="name" placeholder="Enter Your Name : " ref={nameRef}/>
+          <input
+            type="text"
+            id="name"
+            placeholder="Enter Your Name : "
+            ref={nameRef}
+          />
         </div>
         <div>
           <label htmlFor="schoolname">School : </label>
-          <input type="text" id="schoolname" placeholder="Enter Your School Name : " ref={schoolnameRef}/>
+          <input
+            type="text"
+            id="schoolname"
+            placeholder="Enter Your School Name : "
+            ref={schoolnameRef}
+          />
         </div>
         <div>
           <label htmlFor="goal">Goal : </label>
-          <input type="text" id="goal" placeholder="Describe Your Goal : " ref={goalRef}/>
+          <input
+            type="text"
+            id="goal"
+            placeholder="Describe Your Goal : "
+            ref={goalRef}
+          />
         </div>
         <div>
           <label htmlFor="job">Job : </label>
-          <input type="text" id="job" placeholder="Enter Your Job : " ref={jobRef}/>
+          <input
+            type="text"
+            id="job"
+            placeholder="Enter Your Job : "
+            ref={jobRef}
+          />
         </div>
         <div>
           <button onClick={handleSubmit}>Submit</button>

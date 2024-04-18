2bce893e5676706825bcbc00453b16ec2a4bd813
tanvirtbf <tanver018765@gmail.com>
2024-04-18 15:50:45 +0600

    sidebar section added

--- Social Media App/social-media-another/src/App.jsx
+++ Social Media App/social-media-another/src/App.jsx
@@ -2,12 +2,14 @@ import './App.css'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import Header from './components/Header'
 import Footer from './components/Footer'
+import Sidebar from './components/Sidebar'
 
 function App() {
 
   return (
     <>
       <Header />
+      <Sidebar />
       <Footer />
     </>
   )

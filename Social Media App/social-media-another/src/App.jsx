07552e746a942aca6588ec868973377b648aed7b
tanvirtbf <tanver018765@gmail.com>
2024-04-19 21:18:52 +0600

    PostListProvider component wrapped in App.jsx all returning component

--- Social Media App/social-media-another/src/App.jsx
+++ Social Media App/social-media-another/src/App.jsx
@@ -1,26 +1,27 @@
 import "bootstrap/dist/css/bootstrap.min.css";
+import { useState } from "react";
 import "./App.css";
+import CreatePost from "./components/CreatePost";
 import Footer from "./components/Footer";
 import Header from "./components/Header";
-import Sidebar from "./components/Sidebar";
-import CreatePost from "./components/CreatePost";
 import PostList from "./components/PostList";
-import { useState } from "react";
+import Sidebar from "./components/Sidebar";
+import PostListProvider from "./store/post-list-store";
 
 function App() {
-  const [sideBar, setSideBar] = useState('home')
-  console.log(sideBar)
+  const [sideBar, setSideBar] = useState("home");
+  console.log(sideBar);
   return (
-    <>
+    <PostListProvider>
       <div className="app-container">
-        <Sidebar setSideBar={setSideBar} sideBar={sideBar}/>
+        <Sidebar setSideBar={setSideBar} sideBar={sideBar} />
         <div className="content">
           <Header />
-          {sideBar==='home'? <PostList /> : <CreatePost />}
+          {sideBar === "home" ? <PostList /> : <CreatePost />}
           <Footer />
         </div>
       </div>
-    </>
+    </PostListProvider>
   );
 }
 

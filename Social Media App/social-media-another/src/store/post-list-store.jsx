071bcfb3bfc6986fb2549c9e5bea5334a7fc9242
tanvirtbf <tanver018765@gmail.com>
2024-04-19 22:08:34 +0600

    implement reducer function logic

--- Social Media App/social-media-another/src/store/post-list-store.jsx
+++ Social Media App/social-media-another/src/store/post-list-store.jsx
@@ -6,14 +6,25 @@ export const PostList = createContext({
   deletePost: () => {},
 });
 
-const reducer = ()=>{
-  
+const ADDPOST = 'post/add'
+const DELETEPOST = 'post/delete'
+
+const reducer = (state, action)=>{
+  switch(action.type){
+    case ADDPOST:
+      return [...state, action.payload]
+    case DELETEPOST:
+      return state.filter((item) => item.namee !== action.payload)
+    default:
+      return state;
+  }
 }
 
 const PostListProvider = ({ children }) => {
 
   const [postList, dispatchPostList] = useReducer(reducer, []);
   const addPost = (post) => {
+    dispatchPostList(post)
     console.log(post)
   };
   const deletePost = () => {};

5b44791d507bec3df551eb21dabb41d2a41d5e06
tanvirtbf <tanver018765@gmail.com>
2024-04-20 11:15:10 +0600

    Implement fetch posts button

--- Social Media App/social-media-another/src/components/NoPost.jsx
+++ Social Media App/social-media-another/src/components/NoPost.jsx
@@ -1,8 +1,8 @@
-const NoPost = () => {
+const NoPost = ({onFetchPost}) => {
   return (
     <div className='noPost'>
       <h1>Sorry! No Post Here</h1>
-      <button type="button" className="btn btn-primary">Fetch More Posts</button>
+      <button type="button" className="btn btn-primary" onClick={onFetchPost}>Fetch More Posts</button>
     </div>
   )
 }
--- Social Media App/social-media-another/src/components/PostLists.jsx
+++ Social Media App/social-media-another/src/components/PostLists.jsx
@@ -6,9 +6,13 @@ import NoPost from "./NoPost";
 const PostLists = () => {
   const { postList } = useContext(PostListContext);
   console.log(postList)
+
+  const onFetchPost = () =>{
+    console.log('hello')
+  }
   return (
     <div className="postLists">
-      { postList.length ===0 ? <NoPost /> : postList.map((item) => (
+      { postList.length ===0 ? <NoPost onFetchPost={onFetchPost}/> : postList.map((item) => (
         <Post
           key={item.namee}
           namee={item.namee}

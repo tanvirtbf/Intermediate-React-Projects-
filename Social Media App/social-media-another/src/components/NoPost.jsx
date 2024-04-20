fbf7f77ce39361522826af1f17e74e7d7d292429
tanvirtbf <tanver018765@gmail.com>
2024-04-20 11:13:17 +0600

    create fetch post button on NoPost.jsx file

--- Social Media App/social-media-another/src/components/NoPost.jsx
+++ Social Media App/social-media-another/src/components/NoPost.jsx
@@ -2,8 +2,10 @@ const NoPost = () => {
   return (
     <div className='noPost'>
       <h1>Sorry! No Post Here</h1>
+      <button type="button" className="btn btn-primary">Fetch More Posts</button>
     </div>
   )
 }
+
 
 export default NoPost

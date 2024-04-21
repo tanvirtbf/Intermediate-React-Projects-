f5d56926154ff3b2750556e7c14d11bbf6d6803e
tanvirtbf <tanver018765@gmail.com>
2024-04-21 12:45:34 +0600

    start layout routes

--- Social Media App/social-media-another/src/main.jsx
+++ Social Media App/social-media-another/src/main.jsx
@@ -2,11 +2,20 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import CreatePost from './components/CreatePost.jsx'
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <App />
+  },
+  {
+    path: "/",
+    element: <App />
+  },
+  {
+    path: "/create-post",
+    element: <CreatePost />
   }
 ])
 

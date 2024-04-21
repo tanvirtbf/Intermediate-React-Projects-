1b6917cf65c7b1cebee73cc0809d0e0883fc00fb
tanvirtbf <tanver018765@gmail.com>
2024-04-21 12:24:08 +0600

    import router provider

--- Social Media App/social-media-another/src/main.jsx
+++ Social Media App/social-media-another/src/main.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
+import { RouterProvider } from 'react-router-dom'
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
+    <RouterProvider>
+      
+    </RouterProvider>
     <App />
   </React.StrictMode>,
 )

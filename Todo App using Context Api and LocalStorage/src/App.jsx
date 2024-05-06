37aebe10a9132cbbebcc9d888de5ec71830889e6
tanvirtbf <tanver018765@gmail.com>
2024-05-06 14:21:34 +0600

    let's start with localstorage

--- Todo App using Context Api and LocalStorage/src/App.jsx
+++ Todo App using Context Api and LocalStorage/src/App.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import "./App.css";
-import {TodoProvider} from './context'
+import { TodoProvider } from './context';
 
 function App() {
   const [todos,setTodos] = useState([])

8d2da6c92a7785268d164a7449a6b406185959ca
tanvirtbf <tanver018765@gmail.com>
2024-05-06 14:37:46 +0600

    complete localstorage logic

--- Todo App using Context Api and LocalStorage/src/App.jsx
+++ Todo App using Context Api and LocalStorage/src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import "./App.css";
 import { TodoProvider } from './context';
 
@@ -21,6 +21,19 @@ function App() {
   const toggleComplete = (id) => {
     setTodos((prev) => prev.map((prevTodo) => prevTodo.id === id ? {...prevTodo, completed: !prevTodo.completed} : prevTodo))
   }
+
+  useEffect(() => {
+    console.log(JSON.parse(localStorage.getItem("todos")))
+
+    if(todos && todos.length > 0){
+      setTodos(todos)
+    }
+
+  }, [])
+
+  useEffect(() => {
+    localStorage.setItem("todos", JSON.stringify(todos))
+  }, [todos])
 
   return (
     <TodoProvider value={{todos, addTodo, updatedTodo, deletedTodo, toggleComplete}}>

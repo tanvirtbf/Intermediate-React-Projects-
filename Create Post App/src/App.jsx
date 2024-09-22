79ccf69a218513dd36f12cf40be964e5d559dbbe
tanvirtbf <tanver018765@gmail.com>
2024-09-22 14:40:34 +0600

    formData start

--- Create Post App/src/App.jsx
+++ Create Post App/src/App.jsx
@@ -6,15 +6,20 @@ import { useState } from 'react'
 
 function App() {
   const [theme,SetTheme] = useState('home')
-
   function themeChanger(arg){
     SetTheme(arg)
   }
 
+  const [formData,setFormData] = useState([])
+  function addFormData(){
+    console.log('heii')
+  }
+
+
   return (
     <div className={styles.app}>
       <SideBar themeChanger={themeChanger} />
-      {theme==='home'? <Home /> : <CreatePost />}
+      {theme==='home'? <Home /> : <CreatePost addFormData={addFormData} />}
     </div>
   )
 }
--- Create Post App/src/components/createPost/CreatePost.jsx
+++ Create Post App/src/components/createPost/CreatePost.jsx
@@ -1,10 +1,36 @@
 import styles from './CreatePost.module.css'
 
-const CreatePost = () => {
+const CreatePost = ({addFormData}) => {
+  const handleSubmit = (e)=>{
+    e.preventDefault()
+    addFormData()
+  }
   return (
-    <div>
-      Create Post
-    </div>
+    <form className={styles.form} onSubmit={handleSubmit}>
+      <div className={styles.formDiv}>
+        <label htmlFor="name">Name</label>
+        <input type="text" id='name' />
+      </div>
+      <div className={styles.formDiv}>
+        <label htmlFor="age">Age</label>
+        <input type="text" id='age' />
+      </div>
+      <div className={styles.formDiv}>
+        <label htmlFor="university">University</label>
+        <input type="text" id='university' />
+      </div>
+      <div className={styles.formDiv}>
+        <label htmlFor="subject">Subject</label>
+        <input type="text" id='subject' />
+      </div>
+      <div className={styles.formDiv}>
+        <label htmlFor="designation">Designation</label>
+        <input type="text" id='designation' />
+      </div>
+      <div className={styles.formDiv}>
+        <button>Submit</button>
+      </div>
+    </form>
   )
 }
 

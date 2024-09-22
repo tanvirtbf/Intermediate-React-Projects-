09bcd4ee5b67ff7059c1d103637ea74ca92efbaf
tanvirtbf <tanver018765@gmail.com>
2024-09-22 14:12:05 +0600

    sidebar functionality complete

--- Create Post App/src/components/sidebar/SideBar.jsx
+++ Create Post App/src/components/sidebar/SideBar.jsx
@@ -1,10 +1,19 @@
 import styles from './SideBar.module.css'
 
 const SideBar = () => {
+  const handleClick = (id)=>{
+    if(id==='home'){
+      document.querySelector('#home').classList.add(`${styles.bgAdd}`)
+      document.querySelector('#post').classList.remove(`${styles.bgAdd}`)
+    }else {
+      document.querySelector('#home').classList.remove(`${styles.bgAdd}`)
+      document.querySelector('#post').classList.add(`${styles.bgAdd}`)
+    }
+  }
   return (
     <div className={styles.sidebar}>
-      <p>Home</p>
-      <p>Create Post</p>
+      <p id='home' onClick={()=> handleClick('home')}>Home</p>
+      <p id='post' onClick={()=> handleClick('post')}>Create Post</p>
     </div>
   )
 }
